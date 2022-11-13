c6de5c8302157d5245246ade5c73196d41c0daa6
NeHal-AsHfAq12 <110340074+NeHal-AsHfAq12@users.noreply.github.com>
2022-11-13 18:20:08 +0500

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,24 @@
+console.log("hello world");
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/hello ', (req, res) => {
+  res.send('Hello friends chai peelo')
+  console.log(`${req.ip} is saying hello`)
+
+})
+app.get('/bye ', (req, res) => {
+  res.send('Hello friends chai nahi peelo')
+  console.log(`${req.ip} is saying bye`)
+
+})
+app.get('/', (req, res) => {
+  res.send('Hello friends chai to peelo!!')
+  console.log(`${req.ip} is saying nikal bhai`)
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
